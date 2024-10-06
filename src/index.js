e7bed9294dc0265cd8fee523ebe47a3fac71075d
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-10-06 18:32:21 +0300

    Adds ability to type a task, click submit button, view task string and delete task screen

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener(
+    'submit', (e) => {
+      // console.log(e)
+      e.preventDefault()
+      buildToDo(e.target["new-task-description"].value)
+    }
+  )
+
+  function buildToDo(taskDescription) {
+    let p = document.createElement('p')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+    btn.textContent = 'Delete'
+    p.textContent = `${taskDescription} `
+    p.appendChild(btn)
+    // console.log(p)
+    document.querySelector('#tasks').appendChild(p)
+  }
+
+  function handleDelete(e) {
+    e.target.parentNode.remove()
+  }
 });
